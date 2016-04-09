44a2a2d570d2aec5172e7676c2afc23fd33d6dbc
Fotis Evangelou <fevangelou@users.noreply.github.com>
2016-04-09 11:14:07 +0300

    Added styling for module params

--- includes/elements/header.php
+++ includes/elements/header.php
@@ -18,13 +18,8 @@ class JWElementHeader extends JWElement
 	{
 
 		$document = JFactory::getDocument();
-		$document->addStyleDeclaration('
-				.jwHeaderClr { clear:both; height:0; line-height:0; border:none; float:none; background:none; padding:0; margin:0; }
-				.jwHeaderContainer { clear:both; font-weight:bold; font-size:12px; color:#369; margin:12px 0 4px; padding:0; background:#d5e7fa; border-bottom:2px solid #96b0cb; float:left; width:100%; }
-				.jwHeaderContainer15 { clear:both; font-weight:bold; font-size:12px; color:#369; margin:0; padding:0; background:#d5e7fa; border-bottom:2px solid #96b0cb; float:left; width:100%; }
-				.jwHeaderContent { padding:6px 8px; }
-			');
-		if (version_compare(JVERSION, '1.6.0', 'ge'))
+		$document->addStyleSheet(JURI::root(true).'/modules/mod_jw_srfr/includes/elements/header.css');
+		if (version_compare(JVERSION, '2.5.0', 'ge'))
 		{
 			return '<div class="jwHeaderContainer"><div class="jwHeaderContent">'.JText::_($value).'</div><div class="jwHeaderClr"></div></div>';
 		}
